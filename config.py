b85b9a93679d6580c2fc021f8c4aa4c334aa2a75
aying0702 <aying0702@users.noreply.github.com>
2017-10-28 23:03:15 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,9 @@
+CSRF_ENABLED = True
+SECRET_KEY = 'you-will-never-guess'
+
+OPENID_PROVIDERS = [
+    { 'name': 'Google', 'url': 'https://www.google.com/accounts/o8/id' },
+    { 'name': 'Yahoo', 'url': 'https://me.yahoo.com' },
+    { 'name': 'sina', 'url': 'http://sina.com/aying0702@hotmail.com' },
+    { 'name': 'baidu', 'url': 'http://baidu.com/aying0702@hotmail.com' },
+    { 'name': 'MyOpenID', 'url': 'https://www.myopenid.com' }]
