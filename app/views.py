c664e3b01b68a94987bd36ade0145674e73f0575
aying0702 <aying0702@hotmail.com>
2017-10-29 11:14:13 +0800

    database create

--- app/views.py
+++ app/views.py
@@ -1,7 +1,7 @@
 from flask import render_template,flash,redirect
 from app import app
 from .forms import LoginForm
-#@app.route('/')
+@app.route('/')
 @app.route('/index')
 @app.route('/login',methods=['get','post'])
 
