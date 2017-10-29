910386497fb36f94ae1101c6aabc85cb6787fc81
aying0702 <aying0702@hotmail.com>
2017-10-29 11:15:24 +0800

    update

--- config.py
+++ config.py
@@ -1,9 +1,15 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 CSRF_ENABLED = True
 SECRET_KEY = 'you-will-never-guess'
 
 OPENID_PROVIDERS = [
     { 'name': 'Google', 'url': 'https://www.google.com/accounts/o8/id' },
     { 'name': 'Yahoo', 'url': 'https://me.yahoo.com' },
-    { 'name': 'sina', 'url': 'http://sina.com/aying0702@hotmail.com' },
-    { 'name': 'baidu', 'url': 'http://baidu.com/aying0702@hotmail.com' },
+    { 'name': 'AOL', 'url': 'http://openid.aol.com/<username>' },
+    { 'name': 'Flickr', 'url': 'http://www.flickr.com/<username>' },
     { 'name': 'MyOpenID', 'url': 'https://www.myopenid.com' }]
+    
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
